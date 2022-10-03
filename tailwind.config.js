0c3af46c415eb09c098a1449317e3c1cddc6c656
mbrianwebsite <mivanharis@gmail.com>
2022-10-03 20:04:12 +0700

    STYLING NAVBAR

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
-      fontFamily: { poppins: "Poppins" },
+      fontFamily: { poppins: ["Poppins", "sans-serif"] },
+      colors: { primary: "#3258E8" },
+      dropShadow: { cta: "0 4px 4px rgba(0, 0, 0, 0.25)" },
     },
   },
   plugins: [],
